57b2bd61a20e5ad1f504437411028bdc27559418
QuangAnh2702 <97138155+QuangAnh2702@users.noreply.github.com>
2022-01-05 09:01:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "Yêu Con Nợn Linh Lắmmm🐷❤️",
+    introTitle: 'Bae Linh à :33 ',
+    introDesc: `Sao em lại xinh xỉu như thế, em làm anh nghiện em mất òiiii😍`,
+    btnIntro: 'Yêu Linh Lắmmmm^^',
+    title: 'Cho anh hun bae 1 miếng 🥰',
+    desc: 'Nếu hong thích thì có thể trả lại😘😘  ',
+    btnYes: 'Uay em thích nhắm 😍',
+    btnNo: 'Không hun đc-.-',
+    question: 'Em có yêu anh nhiều hongggg',
+    btnReply: 'Gửi choa anh iu <3',
+    reply: 'Yêu anh lắm luôn í, nhiều đến nỗi mất ăn mất ngủ :33',
+    mess: 'Sời anh biết mà 🥰.Anh cũng yêu em nhiều nhiều nhắm 😘😘',
+    messDesc: 'Tối nay hơn 9h mình gặp nhau nha,cho anh hun cái ròi về😁 .',
+    btnAccept: 'Dạ được lun anh êyyy <3',
+    messLink: 'https://www.facebook.com/q.anh.ny.linh'
 }
